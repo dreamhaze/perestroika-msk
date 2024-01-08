1541300b517d58e5f594bf930ff34f992a912e2b
John Doe <johndoe@example.com>
2024-01-08 16:04:52 +0300

    fix build asolute images at runtime

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,15 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Components({ resolvers: [NaiveUiResolver()] }),],
+  plugins: [vue({
+    // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
+    // For example <img src="/images/logo.png"> will not work without the code below
+    template: {
+      transformAssetUrls: {
+        includeAbsolute: false,
+      },
+    },
+  }), Components({ resolvers: [NaiveUiResolver()] }),],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, "src"),
